fa25460f1b167cdd7cc07a1acb26d2ff76fbc399
FrostyFire1 <KamilBaron02@gmail.com>
2022-06-07 00:49:28 +0200

    Add makeGrid(), div for containing grid

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hello!");
+function makeGrid() {
+  let grid = document.querySelector(".grid");
+  console.log(grid);
+  for (let i = 0; i < 16; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < 16; j++) {
+      let cell = document.createElement("div");
+      cell.classList.add("cell");
+      row.appendChild(cell);
+    }
+    grid.appendChild(row);
+  }
+}
+makeGrid();
